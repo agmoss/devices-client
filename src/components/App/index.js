1dc07efcb8618419bdaf4384e5493e3916909cc7
Andrew Moss <agordonmoss@gmail.com>
2019-07-01 09:35:30 -0600

    Removing react defaults

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,20 +7,6 @@ import Main from '../Main'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <select id = "selectStatus"></select>
       <Main/>
     </div>
